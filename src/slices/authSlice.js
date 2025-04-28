4f558bbc2aa6378b95273a45b53219ca805aab8e
anubhavRaj27 <anubhav.27.april@gmail.com>
2025-04-28 23:55:10 +0530

    syntax fix

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,19 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
-    token: null,
-}
+  user: null,
+  token: null,
+};
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
+  name: "auth",
+  initialState,
+  reducers: {
     setLogin: (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-      },
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+  },
+});
 
-})
-
-export const {setLogin} = authSlice.actions;
+export const { setLogin } = authSlice.actions;
 export default authSlice.reducer;
